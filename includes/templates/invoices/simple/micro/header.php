fb17bb8ba46c365fdf6df96436f799d314cee65a
Bas Elbers <baselbers@hotmail.com>
2016-02-01 21:40:19 +0100

    Fixed margin between header and address sections.

--- includes/templates/invoices/simple/micro/header.php
+++ includes/templates/invoices/simple/micro/header.php
@@ -1,11 +1,9 @@
 <table class="company two-column">
-    <tbody>
     <tr>
-        <td class="logo"><?php $this->get_company_logo_html(); ?></td>
-        <td class="info">
+        <td class="logo" width="50%"><?php $this->get_company_logo_html(); ?></td>
+        <td class="info" width="50%">
 	        <?php echo nl2br( $this->template_options['bewpi_company_address'] ); ?><br/>
 	        <?php echo nl2br( $this->template_options['bewpi_company_details'] ); ?>
         </td>
     </tr>
-    </tbody>
 </table>

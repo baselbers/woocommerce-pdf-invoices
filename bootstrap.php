079e152aa05d497930a231ca12ee93e34de85fc0
Bas Elbers <baselbers@hotmail.com>
2016-03-03 23:58:16 +0100

    Improved plugins load order to fix loading problems due to paid version.

--- bootstrap.php
+++ bootstrap.php
@@ -28,8 +28,6 @@ function bewpi_plugins_loaded() {
 	define( 'BEWPI_INVOICES_DIR', $wp_upload_dir['basedir'] . '/bewpi-invoices/' );
 	define( 'BEWPI_LANG_DIR', basename( dirname( __FILE__ ) ) . '/lang' );
 	define( 'BEWPI_LIB_DIR', plugin_dir_path( __FILE__ ) . '/lib/' );
-	define( 'BEWPI_WP_REPO_URL', 'https://wordpress.org/plugins/woocommerce-pdf-invoices/' );
-	define( 'BEWPI_WEBSITE_URL', 'http//wcpdfinvoices.com' );
 
 	require_once( BEWPI_DIR . 'functions.php' );
 	require_once( BEWPI_DIR . 'includes/abstracts/abstract-bewpi-document.php' );
@@ -42,7 +40,7 @@ function bewpi_plugins_loaded() {
 
 	new BE_WooCommerce_PDF_Invoices();
 }
-add_action( 'plugins_loaded', 'bewpi_plugins_loaded', 11 );
+add_action( 'plugins_loaded', 'bewpi_plugins_loaded', 10 );
 
 if ( is_admin() ) {
 	require_once( dirname( __FILE__ ) . '/includes/be-woocommerce-pdf-invoices.php' );

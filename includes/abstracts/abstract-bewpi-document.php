41a6b0eab4b08c4227dd34425373fd68b93bc8c4
Bas Elbers <baselbers@hotmail.com>
2017-01-12 11:55:14 +0100

    Improved code by running WPCS inspection.

--- includes/abstracts/abstract-bewpi-document.php
+++ includes/abstracts/abstract-bewpi-document.php
@@ -1,163 +1,181 @@
 <?php
-if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+/**
+ * Document Class for different types of (invoice) documents.
+ *
+ * @author      Bas Elbers
+ * @category    Abstract Class
+ * @package     BE_WooCommerce_PDF_Invoices/Abstracts
+ * @version     1.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 
 if ( ! class_exists( 'BEWPI_Abstract_Document' ) ) {
+	/**
+	 * Class BEWPI_Abstract_Document.
+	 */
+	abstract class BEWPI_Abstract_Document {
+		/**
+		 * Document filename.
+		 *
+		 * @var string.
+		 */
+		protected $filename;
 
-    abstract class BEWPI_Abstract_Document {
+		/**
+		 * Full path to document.
+		 *
+		 * @var string
+		 */
+		protected $full_path;
 
-	    /**
-	     * @var string
-	     */
-	    protected $filename;
+		/**
+		 * General options.
+		 *
+		 * @var array
+		 */
+		protected $general_options = array();
 
-	    /**
-	     * @var string
-	     */
-	    protected $full_path;
+		/**
+		 * Template options.
+		 *
+		 * @var array
+		 */
+		protected $template_options = array();
 
-        /**
-         * All options from general tab.
-         * @var array
-         */
-        protected $general_options;
+		/**
+		 * BEWPI_Abstract_Document constructor.
+		 */
+		public function __construct() {
+			$this->general_options = get_option( 'bewpi_general_settings' );
+			$this->template_options = get_option( 'bewpi_template_settings' );
+		}
 
-        /**
-         * All options from template tab.
-         * @var array
-         */
-        protected $template_options;
+		/**
+		 * Generate document.
+		 *
+		 * @param array  $html_sections Html output.
+		 * @param string $dest Destination shortcode for file.
+		 * @param bool   $is_paid WooCommerce order paid status.
+		 */
+		protected function generate( $html_sections, $dest, $is_paid ) {
+			require_once BEWPI_DIR . 'lib/mpdf/mpdf.php';
+			require_once BEWPI_DIR . 'lib/mpdf/vendor/autoload.php';
 
-	    /**
-         * Constructor
-         */
-        public function __construct() {
-            $this->general_options      = get_option( 'bewpi_general_settings' );
-            $this->template_options     = get_option( 'bewpi_template_settings' );
-        }
+			$mpdf_params = apply_filters( 'bewpi_mpdf_options', array(
+				'mode'              => '',
+				'format'            => '',
+				'default_font_size' => 0,
+				'default_font'      => 'opensans',
+				'margin_left'       => 14,
+				'margin_right'      => 14,
+				'margin_top'        => 14,
+				'margin_bottom'     => 0,
+				'margin_header'     => 14,
+				'margin_footer'     => 6,
+				'orientation'       => 'P',
+			) );
+			$mpdf         = new mPDF(
+				$mpdf_params['mode'],
+				$mpdf_params['format'],
+				$mpdf_params['default_font_size'],
+				$mpdf_params['default_font'],
+				$mpdf_params['margin_left'],
+				$mpdf_params['margin_right'],
+				$mpdf_params['margin_top'],
+				$mpdf_params['margin_bottom'],
+				$mpdf_params['margin_header'],
+				$mpdf_params['margin_footer'],
+				$mpdf_params['orientation']
+			);
 
-        /**
-         * Generates the invoice with MPDF lib.
-         * @param string $dest
-         * @return string
-         */
-        protected function generate( $html_sections, $dest, $is_paid ) {
-	        set_time_limit(0);
-	        require_once BEWPI_DIR . 'lib/mpdf/mpdf.php';
-	        require_once BEWPI_DIR . 'lib/mpdf/vendor/autoload.php';
-
-	        $mpdf_options = $this->get_mpdf_options();
-	        $mpdf = new mPDF(
-		        $mpdf_options['mode'],               // mode
-		        $mpdf_options['format'],             // format
-		        $mpdf_options['default_font_size'],  // default_font_size
-		        $mpdf_options['default_font'],       // default_font
-		        $mpdf_options['margin_left'],        // margin_left
-		        $mpdf_options['margin_right'],       // margin_right
-		        $mpdf_options['margin_top'],         // margin_top
-		        $mpdf_options['margin_bottom'],      // margin_bottom
-		        $mpdf_options['margin_header'],      // margin_header
-		        $mpdf_options['margin_footer'],      // margin_footer
-		        $mpdf_options['orientation']         // orientation
-	        );
-
-	        // add company logo image as a variable.
-	        $wp_upload_dir = wp_upload_dir();
-	        $image_url = $this->template_options['bewpi_company_logo'];
-	        if ( ! empty( $image_url ) ) {
-		        // use absolute path in order to prevent errors due to wrong (local)host configurations when accessing files and this way `allow_url_fopen` doesn't need to be enabled.
-		        $image_path = str_replace( $wp_upload_dir['baseurl'], $wp_upload_dir['basedir'], $image_url );
-		        $mpdf->company_logo = file_get_contents( $image_path );
-	        }
-
-	        // show paid watermark
-	        if ( (bool)$this->template_options[ 'bewpi_show_payment_status' ] && $is_paid ) {
-		        $mpdf->SetWatermarkText( __( 'Paid', 'woocommerce-pdf-invoices' ) );
-		        $mpdf->showWatermarkText = true;
-				$mpdf->watermarkTextAlpha = "0.2";
-                $mpdf->watermarkImgBehind = false;
-	        }
-
-	        // debugging
-	        if ( (bool) $this->general_options[ 'bewpi_mpdf_debug' ] ) {
-		        $mpdf->debug = true;
-		        $mpdf->showImageErrors = true;
-	        }
-
-	        $mpdf->SetDisplayMode( 'fullpage' );
-	        $mpdf->autoScriptToLang = true;
-	        $mpdf->autoLangToFont = true;
-	        $mpdf->setAutoTopMargin = 'stretch';
-	        $mpdf->setAutoBottomMargin = 'stretch';
-	        $mpdf->autoMarginPadding = 10;
-	        $mpdf->useOnlyCoreFonts = false;
-			$mpdf->useSubstitutions = true;
-
-	        if ( ! empty ( $html_sections['header'] ) ) {
-				$mpdf->SetHTMLHeader($html_sections['header']);
+			// add company logo image as a variable.
+			$wp_upload_dir = wp_upload_dir();
+			$image_url     = $this->template_options['bewpi_company_logo'];
+			if ( ! empty( $image_url ) ) {
+				// use absolute path due to probability of (local)host misconfiguration.
+				// problems with shared hosting when one ip is configured to multiple users/environments.
+				$image_path         = str_replace( $wp_upload_dir['baseurl'], $wp_upload_dir['basedir'], $image_url );
+				$mpdf->company_logo = file_get_contents( $image_path );
 			}
 
-	        if ( ! empty( $html_sections['footer'] ) ) {
-				$mpdf->SetHTMLFooter($html_sections['footer']);
+			// show paid watermark.
+			if ( (bool) $this->template_options['bewpi_show_payment_status'] && $is_paid ) {
+				$mpdf->SetWatermarkText( __( 'Paid', 'woocommerce-pdf-invoices' ) );
+				$mpdf->showWatermarkText  = true;
+				$mpdf->watermarkTextAlpha = '0.2';
+				$mpdf->watermarkImgBehind = false;
 			}
 
-	        $mpdf->WriteHTML( $html_sections['style'] . $html_sections['body'] );
+			// debug.
+			if ( (bool) $this->general_options['bewpi_mpdf_debug'] ) {
+				$mpdf->debug           = true;
+				$mpdf->showImageErrors = true;
+			}
 
-	        $mpdf       = apply_filters( 'bewpi_mpdf', $mpdf );
-			$filename   = ( $dest === 'F' ) ? $this->full_path : $this->filename;
+			$mpdf->SetDisplayMode( 'fullpage' );
+			$mpdf->autoScriptToLang    = true;
+			$mpdf->autoLangToFont      = true;
+			$mpdf->setAutoTopMargin    = 'stretch';
+			$mpdf->setAutoBottomMargin = 'stretch';
+			$mpdf->autoMarginPadding   = 10;
+			$mpdf->useOnlyCoreFonts    = false;
+			$mpdf->useSubstitutions    = true;
 
-	        $mpdf->Output( $filename, $dest );
-        }
+			if ( ! empty( $html_sections['header'] ) ) {
+				$mpdf->SetHTMLHeader( $html_sections['header'] );
+			}
 
-        /**
-         * Get the invoice if exist and show.
-         */
-        public function view() {
-            if ( $this->general_options[ 'bewpi_view_pdf' ] === 'browser' ) {
-	            header( 'Content-type: application/pdf' );
-	            header( 'Content-Disposition: inline; filename = "' . $this->filename . '"' );
-	            header( 'Content-Transfer-Encoding: binary' );
-	            header( 'Content-Length: ' . filesize( $this->full_path ) );
-	            header( 'Accept-Ranges: bytes' );
-	        } else {
-	            header('Content-type: application / pdf');
-	            header('Content-Disposition: attachment; filename="' . $this->filename . '"');
-	            header('Content-Transfer-Encoding: binary');
-	            header('Content-Length: ' . filesize( $this->full_path ));
-	            header('Accept-Ranges: bytes');
-	        }
-	        @readfile( $this->full_path );
-	        exit;
-        }
+			if ( ! empty( $html_sections['footer'] ) ) {
+				$mpdf->SetHTMLFooter( $html_sections['footer'] );
+			}
 
-        /**
-         * Delete PDF file
-         */
-        public function delete() {
-	        return unlink( $this->full_path );
-        }
+			$mpdf->WriteHTML( $html_sections['style'] . $html_sections['body'] );
 
-        /**
-         * Checks if the invoice exists.
-         * @return bool
-         */
-        public function exists() {
-            return file_exists( $this->full_path );
-        }
+			$mpdf     = apply_filters( 'bewpi_mpdf', $mpdf );
+			$filename = ( 'F' === $dest ) ? $this->full_path : $this->filename;
 
-	    private function get_mpdf_options() {
-		    return apply_filters( 'bewpi_mpdf_options', array(
-			    'mode' => '',
-			    'format' => '',
-			    'default_font_size' => 0,
-			    'default_font' => 'opensans',
-			    'margin_left' => 14,
-			    'margin_right' => 14,
-			    'margin_top' => 14,
-			    'margin_bottom' => 0,
-			    'margin_header' => 14,
-			    'margin_footer' => 6,
-			    'orientation' => 'P'
-		    ));
-	    }
-    }
+			$mpdf->Output( $filename, $dest );
+		}
+
+		/**
+		 * View document.
+		 */
+		public function view() {
+			if ( 'browser' === $this->general_options['bewpi_view_pdf'] ) {
+				header( 'Content-type: application/pdf' );
+				header( 'Content-Disposition: inline; filename = "' . $this->filename . '"' );
+				header( 'Content-Transfer-Encoding: binary' );
+				header( 'Content-Length: ' . filesize( $this->full_path ) );
+				header( 'Accept-Ranges: bytes' );
+			} else {
+				header( 'Content-type: application / pdf' );
+				header( 'Content-Disposition: attachment; filename="' . $this->filename . '"' );
+				header( 'Content-Transfer-Encoding: binary' );
+				header( 'Content-Length: ' . filesize( $this->full_path ) );
+				header( 'Accept-Ranges: bytes' );
+			}
+
+			readfile( $this->full_path );
+			exit;
+		}
+
+		/**
+		 * Delete document.
+		 */
+		public function delete() {
+			wp_delete_file( $this->full_path );
+		}
+
+		/**
+		 * Check if document exists.
+		 *
+		 * @return bool
+		 */
+		public function exists() {
+			return file_exists( $this->full_path );
+		}
+	}
 }

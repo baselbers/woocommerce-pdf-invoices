5368166c4d78cb564da5eb7011926ba20f3be087
Bas Elbers <baselbers@hotmail.com>
2017-03-23 17:17:25 +0100

    Improved template by using `printf` for better readability and escaping html.

--- includes/templates/invoice/simple/micro/header.php
+++ includes/templates/invoice/simple/micro/header.php
@@ -1,11 +1,13 @@
 <table class="company two-column">
 	<tr>
 		<td class="logo" width="50%">
-			<?php if ( BEWPI()->templater()->get_logo_url() ) { ?>
-				<img class="company-logo" src="var:company_logo"/>
-			<?php } else { ?>
-				<h1 class="company-logo"><?php echo BEWPI()->templater()->get_option( 'bewpi_company_name' ); ?></h1>
-			<?php } ?>
+			<?php
+			if ( BEWPI()->templater()->get_logo_url() ) {
+				printf( '<img class="company-logo" src="var:company_logo"/>' );
+			} else {
+				printf( '<h1 class="company-logo">%s</h1>', esc_html( BEWPI()->templater()->get_option( 'bewpi_company_name' ) ) );
+			}
+			?>
 		</td>
 		<td class="info small-font" width="50%">
 			<p><?php echo nl2br( BEWPI()->templater()->get_option( 'bewpi_company_address' ) ); ?></p>

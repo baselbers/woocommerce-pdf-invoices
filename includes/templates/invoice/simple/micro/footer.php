7faf961355cd0cb108714c4c9a65316f802e7679
maximvanmaanen <maxim.vanmaanen@gmail.com>
2019-11-14 12:03:59 +0100

    refactoring micro template WIP

--- includes/templates/invoice/simple/micro/footer.php
+++ includes/templates/invoice/simple/micro/footer.php
@@ -7,9 +7,14 @@ $is_theme_text_black = $this->template_options['bewpi_theme_text_black'];
 		<td class="company-details">
 			<p><?php echo nl2br( WPI()->templater()->get_option( 'bewpi_left_footer_column' ) ); ?></p>
 		</td>
+
+        <td class="page-number">
+            <p><?php printf( __( '%1$s of %2$s', 'woocommerce-pdf-invoices' ), '{PAGENO}', '{nbpg}' ); ?></p>
+        </td>
+
 		<td class="payment">
 			<p><?php echo nl2br( WPI()->templater()->get_option( 'bewpi_right_footer_column' ) ); ?></p>
-			<p><?php printf( __( '%1$s of %2$s', 'woocommerce-pdf-invoices' ), '{PAGENO}', '{nbpg}' ); ?></p>
+
 		</td>
 	</tr>
 </table>
--- includes/templates/invoice/simple/micro/header.php
+++ includes/templates/invoice/simple/micro/header.php
@@ -3,7 +3,7 @@
 		<td class="logo" width="50%">
 			<?php
 			if ( WPI()->get_option( 'template', 'company_logo' ) ) {
-				printf( '<img class="company-logo" src="var:company_logo"/>' );
+				printf( '<img class="company-logo" src="var:company_logo" style="max-height:100px;"/>' );
 			} else {
 				printf( '<h1 class="company-logo">%s</h1>', esc_html( WPI()->templater()->get_option( 'bewpi_company_name' ) ) );
 			}

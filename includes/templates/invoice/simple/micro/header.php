06f89ee14d93fe4e549ca5e9bd360bba7d2a3351
Maxim van Maanen <maxim.vanmaanen@gmail.com>
2019-12-03 14:14:21 +0100

    Updated: Micro Template overhaul and deprecated functions replaced

--- includes/templates/invoice/simple/micro/header.php
+++ includes/templates/invoice/simple/micro/header.php
@@ -1,17 +1,37 @@
-<table class="company two-column">
-	<tr>
-		<td class="logo" width="50%">
-			<?php
-			if ( WPI()->get_option( 'template', 'company_logo' ) ) {
-				printf( '<img class="company-logo" src="var:company_logo" style="max-height:100px;"/>' );
-			} else {
-				printf( '<h1 class="company-logo">%s</h1>', esc_html( WPI()->templater()->get_option( 'bewpi_company_name' ) ) );
-			}
-			?>
-		</td>
-		<td class="info small-font" width="50%">
-			<p><?php echo nl2br( WPI()->templater()->get_option( 'bewpi_company_address' ) ); ?></p>
-			<p><?php echo nl2br( WPI()->templater()->get_option( 'bewpi_company_details' ) ); ?></p>
-		</td>
-	</tr>
-</table>
+<?php
+/**
+ * PDF invoice header template that will be visible on every page.
+ *
+ * This template can be overridden by copying it to youruploadsfolder/woocommerce-pdf-invoices/templates/invoice/simple/yourtemplatename/header.php.
+ *
+ * HOWEVER, on occasion WooCommerce PDF Invoices will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @author  Bas Elbers
+ * @package WooCommerce_PDF_Invoices/Templates
+ * @version 0.0.1
+ */
+?>
+
+<div class="top">
+	<table>
+		<tr>
+			<td class="logo">
+				<?php
+				if ( WPI()->get_option( 'template', 'company_logo' ) ) {
+					printf( '<img class="company-logo" src="var:company_logo" style="max-height:150px;"/>' );
+				} else {
+					printf( '<h1 class="company-name">%s</h1>', esc_html( WPI()->templater()->get_option( 'bewpi_company_name' ) ) );
+				}
+				?>
+			</td>
+			<td class="info">
+				<p><?php echo nl2br( WPI()->templater()->get_option( 'bewpi_company_address' ) ); ?></p>
+				<p><?php echo nl2br( WPI()->templater()->get_option( 'bewpi_company_details' ) ); ?></p>
+			</td>
+		</tr>
+	</table>
+</div>

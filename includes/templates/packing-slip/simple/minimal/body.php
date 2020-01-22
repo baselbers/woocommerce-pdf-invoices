b4ded5a9fa23220e8159f275497e9e4901bd3e96
Bas Elbers <baselbers@hotmail.com>
2020-01-22 18:41:53 +0100

    Improved template by using markup from invoice.

--- includes/templates/packing-slip/simple/minimal/body.php
+++ includes/templates/packing-slip/simple/minimal/body.php
@@ -1,129 +1,153 @@
 <?php
-$templater           = WPI()->templater();
-$packing_slip        = $templater->packing_slip;
-$theme_color         = WPI()->get_option( 'template', 'color_theme' );
-$is_theme_text_black = WPI()->get_option( 'template', 'theme_text_black' );
-$line_items          = $packing_slip->order->get_items( 'line_item' );
+/**
+ * PDF packing slip template body.
+ *
+ * This template can be overridden by copying it to youruploadsfolder/woocommerce-pdf-invoices/templates/packing-slip/simple/yourtemplatename/body.php.
+ *
+ * HOWEVER, on occasion WooCommerce PDF Invoices will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @author  Bas Elbers
+ * @package WooCommerce_PDF_Invoices/Templates
+ * @version 0.0.1
+ */
+
+$packing_slip           = WPI()->templater()->packing_slip;
+$theme_color_text       = WPI()->get_option( 'template', 'color_theme_text' );
+$theme_color_background = WPI()->get_option( 'template', 'color_theme_background' );
+$line_items             = $packing_slip->order->get_items( 'line_item' );
 
 // Header and footer margin and padding.
 $this->mpdf->setAutoTopMargin    = 'stretch';
 $this->mpdf->setAutoBottomMargin = 'stretch';
 $this->mpdf->autoMarginPadding   = 25; // mm.
 ?>
-<div class="body">
-	<table class="customer-address">
-		<tbody>
-		<tr>
 
-			<td class="title-information">
-				<h1 class="title"><?php esc_attr_e( 'Packing Slip', 'woocommerce-pdf-invoices' ); ?></h1>
-				<?php
-				printf( __( 'Order Date: %s', 'woocommerce-pdf-invoices' ), $packing_slip->get_formatted_order_date() );
-				printf( '<br />' );
-				printf( __( 'Order Number: %s', 'woocommerce-pdf-invoices' ), $packing_slip->order->get_order_number() );
+<div class="title">
+	<h2><?php esc_attr_e( 'Packing Slip', 'woocommerce-pdf-invoices' ); ?></h2>
+</div>
+<table>
+	<tr class="information">
+		<td class="primary">
+			<?php
+			printf( __( 'Order Date: %s', 'woocommerce-pdf-invoices' ), $packing_slip->get_formatted_order_date() );
+			echo '<br>';
+			printf( __( 'Order Number: %s', 'woocommerce-pdf-invoices' ), $packing_slip->order->get_order_number() );
 
-				$shipping_method = $packing_slip->order->get_shipping_method();
-				if ( $shipping_method ) {
-					printf( '<br />' );
-					printf( __( 'Shipping Method: %s', 'woocommerce-pdf-invoices' ), $shipping_method );
-				}
+			$shipping_method = $packing_slip->order->get_shipping_method();
+			if ( $shipping_method ) {
+				echo '<br>';
+				printf( __( 'Shipping Method: %s', 'woocommerce-pdf-invoices' ), $shipping_method );
+			}
 
-				$payment_method = $packing_slip->order->get_payment_method_title();
-				if ( $payment_method ) {
-					printf( '<br />' );
-					printf( __( 'Payment Method: %s', 'woocommerce-pdf-invoices' ), $payment_method );
-				}
-				?>
-			</td>
-			<td class="bill-to">
-				<?php
-				printf( '<strong>%s</strong><br />', esc_html__( 'Bill to:', 'woocommerce-pdf-invoices' ) );
-				echo $packing_slip->order->get_formatted_billing_address();
+			$payment_method = $packing_slip->order->get_payment_method_title();
+			if ( $payment_method ) {
+				echo '<br>';
+				printf( __( 'Payment Method: %s', 'woocommerce-pdf-invoices' ), $payment_method );
+			}
+			?>
+		</td>
 
-				do_action( 'wpi_after_formatted_billing_address', $packing_slip );
-				?>
-			</td>
-			<td class="ship-to">
-				<?php
-				printf( '<strong>%s</strong><br />', esc_html__( 'Ship to:', 'woocommerce-pdf-invoices' ) );
-				echo $packing_slip->order->get_formatted_shipping_address();
+		<td class="bill-to">
+			<?php
+			printf( '<strong>%s</strong><br>', esc_html__( 'Bill to:', 'woocommerce-pdf-invoices' ) );
+			echo $packing_slip->order->get_formatted_billing_address();
+
+			do_action( 'wpi_after_formatted_billing_address', $packing_slip );
+			?>
+		</td>
+
+		<td class="ship-to">
+			<?php
+			$formatted_shipping_address = $packing_slip->order->get_formatted_shipping_address();
+			if ( WPI()->get_option( 'template', 'show_ship_to' ) && ! WPI()->has_only_virtual_products( $packing_slip->order ) && ! empty( $formatted_shipping_address ) ) {
+				printf( '<strong>%s</strong><br>', esc_html__( 'Ship to:', 'woocommerce-pdf-invoices' ) );
+				echo $formatted_shipping_address;
 
 				do_action( 'wpi_after_formatted_shipping_address', $packing_slip );
-				?>
-			</td>
-		</tr>
-		</tbody>
-	</table>
-	<table class="products">
-		<thead>
-		<tr class="heading" style="background-color: <?php echo esc_attr( $theme_color ); ?>">
-			<th>
-				<?php esc_html_e( 'SKU', 'woocommerce-pdf-invoices' ); ?>
-			</th>
-
-			<th>
-				<?php esc_html_e( 'Product', 'woocommerce-pdf-invoices' ); ?>
-			</th>
-
-			<th>
-				<?php esc_html_e( 'Quantity', 'woocommerce-pdf-invoices' ); ?>
-			</th>
-		</tr>
-		</thead>
-		<tbody>
-		<?php
-		foreach ( $line_items as $item_id => $item ) {
-			/** @var WC_Product $product */
-			$product = $item->get_product();
+			}
 			?>
+		</td>
+	</tr>
+</table>
 
-			<tr class="item">
-				<td width="20%">
-					<?php echo $product && $product->get_sku() ? esc_html( $product->get_sku() ) : '-'; ?>
-				</td>
+<table>
+	<thead>
+	<tr class="heading" style="background-color:<?php echo esc_attr( $theme_color_background ); ?>;">
+		<th style="color:<?php echo esc_attr( $theme_color_text ); ?>;">
+			<?php esc_html_e( 'SKU', 'woocommerce-pdf-invoices' ); ?>
+		</th>
 
-				<td width="65%">
-					<?php
-					echo esc_html( $item['name'] );
+		<th style="color:<?php echo esc_attr( $theme_color_text ); ?>;">
+			<?php esc_html_e( 'Product', 'woocommerce-pdf-invoices' ); ?>
+		</th>
 
-					do_action( 'wpi_order_item_meta_start', $item, $this->order );
-					do_action( 'woocommerce_order_item_meta_start', $item_id, $item, $this->order );
+		<th style="color:<?php echo esc_attr( $theme_color_text ); ?>;">
+			<?php esc_html_e( 'Quantity', 'woocommerce-pdf-invoices' ); ?>
+		</th>
+	</tr>
+	</thead>
+	<tbody>
+	<?php
+	/**
+	 * Item annotation.
+	 *
+	 * @var WC_Order_Item_Product $item
+	 */
+	foreach ( $line_items as $item_id => $item ) {
+		/**
+		 * Product annotation.
+		 *
+		 * @var WC_Product $product
+		 */
+		$product = $item->get_product();
+		?>
 
-					WPI()->templater()->display_item_meta( $item );
+		<tr class="item">
+			<td>
+				<?php echo $product && $product->get_sku() ? esc_html( $product->get_sku() ) : '-'; ?>
+			</td>
 
-					do_action( 'woocommerce_order_item_meta_end', $item_id, $item, $this->order );
-					?>
-				</td>
-
-				<td width="15%">
-					<?php echo esc_html( $item['qty'] ); ?>
-				</td>
-			</tr>
-
-		<?php } ?>
-		</tbody>
-	</table>
-
-	<table class="customer-notes">
-		<!-- Notes & terms -->
-		<tr>
-			<td class="border">
+			<td>
 				<?php
-				// Customer notes.
-				if ( WPI()->get_option( 'template', 'show_customer_notes' ) ) {
-					// Note added by customer.
-					$customer_note = BEWPI_WC_Order_Compatibility::get_customer_note( $packing_slip->order );
-					if ( $customer_note ) {
-						printf( '<strong>' . __( 'Note from customer: %s', 'woocommerce-pdf-invoices' ) . '</strong><br>', nl2br( $customer_note ) );
-					}
+				echo esc_html( $item['name'] );
 
-					// Notes added by administrator on 'Edit Order' page.
-					foreach ( $packing_slip->order->get_customer_order_notes() as $custom_order_note ) {
-						printf( '<strong>' . __( 'Note to customer: %s', 'woocommerce-pdf-invoices' ) . '</strong><br>', nl2br( $custom_order_note->comment_content ) );
-					}
-				}
+				do_action( 'woocommerce_order_item_meta_start', $item_id, $item, $this->order );
+				WPI()->templater()->display_item_meta( $item );
+				do_action( 'woocommerce_order_item_meta_end', $item_id, $item, $this->order );
 				?>
 			</td>
+
+			<td>
+				<?php echo esc_html( $item['qty'] ); ?>
+			</td>
 		</tr>
-	</table>
-</div>
+
+	<?php } ?>
+	</tbody>
+</table>
+
+<table class="notes">
+	<tr>
+		<td>
+			<?php
+			// Customer notes.
+			if ( WPI()->get_option( 'template', 'show_customer_notes' ) ) {
+				// Note added by customer.
+				$customer_note = BEWPI_WC_Order_Compatibility::get_customer_note( $packing_slip->order );
+				if ( $customer_note ) {
+					printf( '<strong>' . __( 'Note from customer: %s', 'woocommerce-pdf-invoices' ) . '</strong><br>', nl2br( $customer_note ) );
+				}
+
+				// Notes added by administrator on 'Edit Order' page.
+				foreach ( $packing_slip->order->get_customer_order_notes() as $custom_order_note ) {
+					printf( '<strong>' . __( 'Note to customer: %s', 'woocommerce-pdf-invoices' ) . '</strong><br>', nl2br( $custom_order_note->comment_content ) );
+				}
+			}
+			?>
+		</td>
+	</tr>
+</table>
--- includes/templates/packing-slip/simple/minimal/header.php
+++ includes/templates/packing-slip/simple/minimal/header.php
@@ -1,23 +1,35 @@
-<div class="header">
-	<table class="company">
-		<tr>
-			<td class="logo">
-				<?php
-				if ( WPI()->get_option( 'template', 'company_logo' ) ) {
-					printf( '<img class="company-logo" src="var:company_logo" style="max-height:150px;"/>' );
-				} else {
-					printf( '<h1 class="company-logo">%s</h1>', esc_html( WPI()->get_option( 'template', 'company_name' ) ) );
-				}
-				?>
-			</td>
-			<td class="address">
-				<p>
-					<?php
-					echo WPI()->get_formatted_company_address() . '<br>';
-					echo WPI()->get_formatted_company_details();
-					?>
-				</p>
-			</td>
-		</tr>
-	</table>
-</div>
+<?php
+/**
+ * PDF packing slip header template that will be visible on every page.
+ *
+ * This template can be overridden by copying it to youruploadsfolder/woocommerce-pdf-invoices/templates/packing-slip/simple/yourtemplatename/header.php.
+ *
+ * HOWEVER, on occasion WooCommerce PDF Invoices will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @author  Bas Elbers
+ * @package WooCommerce_PDF_Invoices/Templates
+ * @version 0.0.1
+ */
+
+?>
+
+<table class="top">
+	<tr>
+		<td class="logo">
+			<?php
+			if ( WPI()->get_option( 'template', 'company_logo' ) ) {
+				printf( '<img src="var:company_logo" style="max-height:150px;"/>' );
+			} else {
+				printf( '<h2>%s</h2>', esc_html( WPI()->get_option( 'template', 'company_name' ) ) );
+			}
+			?>
+		</td>
+		<td>
+			<?php echo WPI()->get_formatted_company_address() . '<br>' . WPI()->get_formatted_company_details(); ?>
+		</td>
+	</tr>
+</table>
